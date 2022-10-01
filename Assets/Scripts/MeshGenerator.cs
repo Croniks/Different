5a9e5ba17b973f2e8e6be175a1851554d7ade8c3
Croniks <wowmandarin@yandex.ru>
2022-10-01 19:33:45 +0300

    Added CheckVisibility util

---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+
+namespace Assets.Scripts
+{
+    public class MeshGenerator : MonoBehaviour
+    {
+        [SerializeField] private int _xSize, _ySize;
+        private Vector3[] _vertices;
+        private MeshFilter _meshFilter;
+        private MeshRenderer _meshRenderer;
+
+        
+        private void Start()
+        {
+            GenerateQuad();
+        }
+        
+        private void GenerateQuad()
+        {
+            _vertices = new Vector3[(_xSize + 1) * (_ySize + 1)];
+
+            for(int i = 0, y = 0; y < _ySize; y++)
+            {
+                for(int x = 0; x < _xSize; x++, i++)
+                {
+                    _vertices[i] = new Vector3(x, y);
+                }
+            }
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if(_vertices == null)
+            {
+                return;
+            }
+
+            Gizmos.color = Color.green;
+
+            foreach(var vertex in _vertices)
+            {
+                Gizmos.DrawSphere(vertex, 0.2f);
+            }
+        }
+    }
+}
